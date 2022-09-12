eddb4d23873671fda5b5eeac5a8a891559c54893
Will Cater <wcater@redhat.com>
2022-09-12 12:02:44 -0400

    RHCLOUD-21024: Added/modified test for more use cases

--- test/test.js
+++ test/test.js
@@ -11,6 +11,10 @@ chai.use(chaiJSON);
 
 const url= 'http://localhost:8090';
 
+let jdoeUser = {
+  name: "jdoe",
+  password: "BOO"
+};
 
 /*
  * Test / route
@@ -32,11 +36,12 @@ describe('/GET /',() => {
  */
 
 describe('/GET /v1/users',() => {
-    it('should return 405', (done) => {
+    it('should return 405 with method not allowed', (done) => {
         chai.request(url)
             .get('/v1/users')
             .end((err,res) => {
                 res.should.have.status(405);
+                expect(res.text).to.contain("method not allowed");
             done();
         });
     });
@@ -89,21 +94,6 @@ describe('/POST /v1/users',() => {
 });
 
 
-
-describe('/POST /v1/users',() => {
-    it.skip("should find users even without any parameter", (done) => {
-        chai.request(url)
-            .post('/v1/users')
-            .end((err,res) => {
-                res.should.have.status(200);
-                JSON_response = JSON.parse(res.text);
-                expect(JSON_response).not.be.empty;
-            done();
-        });
-    });
-});
-
-
 describe('/POST /v1/users',() => {
     it("should find users for multiple usernames for all of them that exist", (done) => {
         chai.request(url)
@@ -124,13 +114,12 @@ describe('/POST /v1/users',() => {
  * Test /v1/jwt
  */
 describe('/POST /v1/jwt',() => {
-    it.skip("should get the jwt auth token", (done) => {
+    it("should get the jwt auth token", (done) => {
         chai.request(url)
             .post('/v1/jwt')
             .end((err,res) => {
                 res.should.have.status(200);
-                JSON_response = JSON.parse(res.text);
-                expect(JSON_response).not.be.empty;
+                expect(res.text).not.be.empty;
             done();
         });
     });
@@ -197,3 +186,28 @@ describe('/GET /v3/accounts',() => {
 /*
  * Test /api/entitlements/v1/services
  */
+describe('/GET /api/entitlements/v1/services',() => {
+    it('should return the users entitlements', (done) => {
+        chai.request(url)
+            .get('/api/entitlements/v1/services')
+            .auth(jdoeUser.name, jdoeUser.password)
+            .end((err,res) => {
+                res.should.have.status(200);
+                JSON_response = JSON.parse(res.text);
+                expect(JSON_response.ansible["is_entitled"]).to.be.true;
+            done();
+        });
+    });
+});
+
+
+describe('/GET /api/entitlements/v1/services without auth',() => {
+    it('should return the users entitlements', (done) => {
+        chai.request(url)
+            .get('/api/entitlements/v1/services')
+            .end((err,res) => {
+                res.should.have.status(403);
+            done();
+        });
+    });
+});
