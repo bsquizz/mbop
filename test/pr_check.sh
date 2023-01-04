bde36ff8519177e1f5b3cda9cdaaa67c7871fe6a
Brandon Squizzato <bsquizza@redhat.com>
2023-01-04 10:13:13 -0500

    Do not use deploy_ephemeral_env.sh as written

--- test/pr_check.sh
+++ test/pr_check.sh
@@ -14,13 +14,8 @@ IQE_CJI_TIMEOUT="10m"  # This is the time to wait for smoke test to complete or 
 DEPLOY_FRONTENDS="true"
 IQE_SELENIUM="true"
 IQE_ENV="ephemeral"
-
-# Use stage components for this check
 REF_ENV="insights-stage"
-
-# Override the '--set-template-ref' arg that 'deploy_ephemeral_env.sh' will use. If f we do not do
-# this, bonfire tries to set the rbac template ref to be this PR's git ref which will be invalid
-EXTRA_DEPLOY_ARGS="--set-template-ref rbac=master"
+NAMESPACE_POOL="default"
 
 # Install bonfire repo/initialize
 CICD_URL=https://raw.githubusercontent.com/RedHatInsights/bonfire/master/cicd
@@ -29,13 +24,34 @@ curl -s $CICD_URL/bootstrap.sh > .cicd_bootstrap.sh && source .cicd_bootstrap.sh
 # unit tests handled by gh actions ...
 #source $CICD_ROOT/unit_test.sh
 
+# Build pr check quay image
 source $CICD_ROOT/build.sh
-source $CICD_ROOT/deploy_ephemeral_env.sh
+
+# Do not 'source $CICD_ROOT/deploy_ephemeral_env.sh' here because this is a slightly different
+# test scenario where we cannot override the mbop image tag at deployment time, run our own
+# deploy command ...
+source ${CICD_ROOT}/_common_deploy_logic.sh
+
+set -x
+export BONFIRE_NS_REQUESTER="${JOB_NAME}-${BUILD_NUMBER}"
+export NAMESPACE=$(bonfire namespace reserve --pool ${NAMESPACE_POOL})
+SMOKE_NAMESPACE=$NAMESPACE  # track which namespace was used here for 'teardown' in common_deploy_logic
+
+bonfire deploy \
+    ${APP_NAME} \
+    --source=appsre \
+    --ref-env ${REF_ENV} \
+    --namespace ${NAMESPACE} \
+    --timeout ${DEPLOY_TIMEOUT} \
+    --frontends ${DEPLOY_FRONTENDS} \
+    ${COMPONENTS_ARG} \
+    ${COMPONENTS_RESOURCES_ARG} \
+set +x
 
 # Update mbop in this environment to use the newly built PR image
 CLOWDENV_NAME="env-$NAMESPACE"
 kubectl patch clowdenvironment ${CLOWDENV_NAME} --type='merge' -p '{"spec":{"providers":{"web":{"images":{"mockBop":"'${IMAGE}:${IMAGE_TAG}'"}}}}}'
 kubectl rollout status deployment/${CLOWDENV_NAME}-mbop
 
+# Run rbac test
 source $CICD_ROOT/cji_smoke_test.sh
-
